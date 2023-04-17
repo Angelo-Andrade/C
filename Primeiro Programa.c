71cbbe8cae9a9d6a80d46eb9cf1190c1e60dd897
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2023-04-16 21:14:51 -0300

    Add files via upload

---
+++ Primeiro Programa.c
@@ -0,0 +1,22 @@
+#include <stdio.h> //biblioteca da linguagem C, alem dessa existe stdlib.h, math.h, locale.h
+// \n pula linha
+// void tambem funciona apesar de que alguns operadores n�o inclu�rem essa palavra
+int main()
+{
+    printf("Ola mundo!\n");
+    printf("Meu nome aqui: ");
+    printf("Angelo.\n\n");
+
+
+
+
+// � necess�rio sinalizar o tipo de vari�vel (sinalizadores: %c, %i, %f, %lf)
+
+
+
+
+    int idademedia = 25;
+    printf("Idade media dos alunos do curso e: %i", idademedia);
+
+
+}
