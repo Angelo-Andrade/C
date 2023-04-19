3de2eaca1ebf7045ee3a680d843638cd75b10017
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2023-04-19 17:38:07 -0300

    Add files via upload

---
+++ exercicio006.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <math.h>
+
+
+
+int main(){
+    double R, areaN, comprimentoN;
+ 
+        printf("Para efetuar as contas, qual o valor do raio da circunferencia?\n");
+            scanf("%lf", &R);
+    areaN = 3.14*R;
+        printf("A area equivale, aproximadamente, a: %lf unidades de medida de distancia.\n", areaN);
+    comprimentoN = 2*3.14*R;
+        printf("O comprimento equivale, aproximadamente, a: %lf unidades de medidade de distancia.\n", comprimentoN);
+
+}
