35652b3667cc7e55ab7a65b7d4db8146ff3b7fcb
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2023-04-19 16:42:16 -0300

    Add files via upload

---
+++ matematica.c
@@ -0,0 +1,16 @@
+//funcoes matematicas
+#include <math.h>
+
+floor(10.4); //arredonda para baixo
+ceil(10.4);  //arredonda para cima
+sqrt(4);  //Calcula raiz quadrada
+pow(base, expoente); //Calcula a potenciacao
+
+
+
+// operacaes
+// + soma
+// - subtracao
+// * multiplicacao
+// / divisao
+// % resto
