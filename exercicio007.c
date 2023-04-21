c95b58e382d71c2e6e42fc19121ded88f56c27a3
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2023-04-21 02:01:49 -0300

    Update exercicio007.c

--- exercicio007.c
+++ exercicio007.c
@@ -7,15 +7,15 @@ int main(){
     float horaExt, valorHoraTrab, valorHoraExt;
     double valorMes;
 
-        printf("Me conte, qual o valor da sua hora de trabalho?\n");
-            scanf("%f", &valorHoraTrab);
-        printf("Agora, por favor diga quantas horas de trabalho extra voce fez este mes:\n");
-            scanf("%f", &horaExt);    
+    printf("Me conte, qual o valor da sua hora de trabalho?\n");
+    scanf("%f", &valorHoraTrab);
+    printf("Agora, por favor diga quantas horas de trabalho extra voce fez este mes:\n");
+    scanf("%f", &horaExt);    
     
     valorHoraExt = ((0.5*valorHoraTrab)+valorHoraTrab)*horaExt;
     valorMes = (valorHoraTrab*40*4)+(valorHoraExt);
 
-        printf("Aproximadamente, o salario mensal da sua jornada de trabalho e: R$%lf \n", valorMes);
+    printf("Aproximadamente, o salario mensal da sua jornada de trabalho e: R$%lf \n", valorMes);
 
 
 
