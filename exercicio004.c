608b1ca57f6eb5fab6c4f0a73f687aa99d0ede80
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2023-04-21 02:00:35 -0300

    Update exercicio004.c

--- exercicio004.c
+++ exercicio004.c
@@ -5,13 +5,13 @@ int main()
 {
     
     double y, z, potenciay, potenciaz, soma;
-        printf("A funcao e: x = y^2+z^3+1\n");
-        printf("Diga dois valores para calcular o valor de X\n");
-            scanf("%lf", &y);
-            scanf("%lf", &z);
+    printf("A funcao e: x = y^2+z^3+1\n");
+    printf("Diga dois valores para calcular o valor de X\n");
+    scanf("%lf", &y);
+    scanf("%lf", &z);
         
     
     potenciay = pow(y, 2);
     potenciaz = pow(z, 3);
     soma = potenciay+potenciaz+1; 
-        printf("%f", soma);
+    printf("%f", soma);
