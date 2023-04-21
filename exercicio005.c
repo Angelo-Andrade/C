5c0b1777efbfa6b71668d994afed123999c1b9c7
Ângelo <121000234+Angelo-Andrade@users.noreply.github.com>
2023-04-21 02:01:05 -0300

    Update exercicio005.c

--- exercicio005.c
+++ exercicio005.c
@@ -3,13 +3,13 @@
 
 int main(){
 //soma de duas raizes de numeros adquiridos
-double m, n, raiz_quadradam, raiz_quadradan, soma;
+    double m, n, raiz_quadradam, raiz_quadradan, soma;
     printf("Diga dois valores para efetuar uma soma de raizes:\n");
-        scanf("%lf", &m);
-        scanf("%lf", &n);
-raiz_quadradam = sqrt(m);
-raiz_quadradan = sqrt(n);
-soma = raiz_quadradam+raiz_quadradan;
+    scanf("%lf", &m);
+    scanf("%lf", &n);
+    raiz_quadradam = sqrt(m);
+    raiz_quadradan = sqrt(n);
+    soma = raiz_quadradam+raiz_quadradan;
     printf("O valor da soma das raizes e: %f", soma);
 
     
